1b8f540e7019b0026aa6b8f09b6a891943c4b5b2
Vladislav <kovleshenko99@mail.ru>
2023-11-06 10:32:16 +0400

    added hmr, updated pnpm

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: false});
   app.enableCors({credentials: true, origin: true})
@@ -23,5 +25,10 @@ async function bootstrap() {
   const PORT = process.env.PORT || 4000
 
   await app.listen(PORT);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
