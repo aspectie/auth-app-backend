fb79e7b48a734b61394d6e7cd77c65e98925b387
Vladislav <kovleshenko99@mail.ru>
2023-11-21 16:26:59 +0400

    empty fields validation fixed

--- src/collections/dto/create-collection.dto.ts
+++ src/collections/dto/create-collection.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
 
 export class CreateCollectionDto {
   @ApiProperty({required: true})
+  @IsNotEmpty()
   readonly title: string
 
   @ApiProperty({required: true})
+  @IsNotEmpty()
   readonly description: string
-
+  
   @ApiProperty({required: true})
+  @IsNotEmpty()
   readonly theme: string
 
   @ApiProperty({required: true})
+  @IsNotEmpty()
   readonly user: string
 }
